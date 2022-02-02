ba438d74c162727eea987274551e5e360eafccd3
Tales <20765261+tales-br@users.noreply.github.com>
2022-02-02 16:12:23 -0300

    Alterado: ex3.js

--- JS/4-Lista/ex3.js
+++ JS/4-Lista/ex3.js
@@ -4,7 +4,7 @@
 
 //Vou usar o arrow function para fixar o conceito
 
-let elevaValor = (x=1, y=1) => Math.pow(x, y)
+let elevaValor = (x=1, y=1) => Math.pow(x, y) //ou usando a expressao "**" -> x ** y
 
 console.log(elevaValor(2, 3))
 console.log(elevaValor(2, 0))
