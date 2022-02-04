22801f0285f4464705d44629f590345c28f46694
Tales <20765261+tales-br@users.noreply.github.com>
2022-02-04 17:31:49 -0300

    Adicionado: 5-OO/oo.js

---
+++ JS/5-OO/oo.js
@@ -0,0 +1,26 @@
+//NÃO EXECUTAVEL, CONCEITUAL!!
+
+//Paradigma Procedural
+processamento (val1, val2, val3)
+
+//OO
+objeto =
+{
+    val1,
+    val2,
+    val3,
+    processamento()
+    {
+        //...
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto (dados)
+
+/*
+PRINCÍPIOS IMPORTANTES:
+1- ABSTRAÇÃO
+2- ENCAPSULAMENTO
+3- HERANÇA
+4- POLIMORFISMO
+*/
