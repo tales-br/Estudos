7797dd9a0819f9ddea7f43f4db9f282c1ad948ec
Tales <20765261+tales-br@users.noreply.github.com>
2022-02-28 19:56:37 -0300

    Retirado código com erro

--- JS/6-Array/map2.js
+++ JS/6-Array/map2.js
@@ -10,13 +10,6 @@ const toObj = json => JSON.parse(json)
 const soPreco = produto => produto.preco
 
 const compras = carrinho.map(toObj).map(soPreco)
-const valor = compras.forEach(function (e, index, array, valorTotal = 0)
-{
-   // valorTotal = valorTotal + e
-    //console.log(valorTotal)
-        return valorTotal+e
-    
-})
 
 console.log(valor)
 
