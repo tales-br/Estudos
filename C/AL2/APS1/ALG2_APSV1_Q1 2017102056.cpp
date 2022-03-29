dbb8fd51ff687b47ea70a91cbcf917ec41bc39de
Tales <20765261+tales-br@users.noreply.github.com>
2022-03-29 13:57:44 -0300

    Adicionado: Branch C / Adicionado: ALG2_APSV1_Q1 2017102056.cpp

---
+++ C/AL2/APS1/ALG2_APSV1_Q1 2017102056.cpp
@@ -0,0 +1,19 @@
+/*
+01- Elabore um algoritmo que leia um conjunto de 10 dados numéricos inteiros e gere dois vetores,
+o pr imeiro (vetor XX) contendo os elementos pares e o segundo
+(vetor YY) contendo os elementos ímpares.
+*/
+
+/*
+Author: Tales Barbosa Rodrigues
+Github: https://github.com/tales-br/Estudos/C
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+}
