07fcc92d16c313bba13a5eaa7f97f59f6aaa643a
Tales <20765261+tales-br@users.noreply.github.com>
2022-04-01 17:35:36 -0300

    Programacao Estrutura

--- C/ESTRUTURA_DE_DADOS/APS1/SOURCE/ESTR_APSV1_2017102056 (SIMPLES).cpp
+++ C/ESTRUTURA_DE_DADOS/APS1/SOURCE/ESTR_APSV1_2017102056 (SIMPLES).cpp
@@ -2,8 +2,8 @@
 
 /*
 01- Elabore um algoritmo em DEV C++ (C) que possibilite montar uma LISTA, uma
-FILA e uma PILHA. O usuário escolhe que estrutura deseja montar.
-IMPLEMENTAÇÃO - VETOR DE 10 POSIÇÕES DE INTEIRO.
+FILA e uma PILHA. O usuï¿½rio escolhe que estrutura deseja montar.
+IMPLEMENTAï¿½ï¿½O - VETOR DE 10 POSIï¿½ï¿½ES DE INTEIRO.
 */
 
 
@@ -11,20 +11,20 @@ IMPLEMENTAÇÃO - VETOR DE 10 POSIÇÕES DE INTEIRO.
 //"REQUISITOS"
 
 
-	//ESPECIFICAÇÃO DAS ESTRUTURAS
+	//ESPECIFICAï¿½ï¿½O DAS ESTRUTURAS
 	
 		
 		//LISTA
 		
 			/*
-			Operações
-			• Inserção - ordenada de acordo com o valor inserido
-			• Exclusão - realoca os valores da LISTA após a exclusão. O usuário informa o
-			valor a ser excluído.
+			Operaï¿½ï¿½es
+			ï¿½ Inserï¿½ï¿½o - ordenada de acordo com o valor inserido
+			ï¿½ Exclusï¿½o - realoca os valores da LISTA apï¿½s a exclusï¿½o. O usuï¿½rio informa o
+			valor a ser excluï¿½do.
 			
 			Controles
-			• Inclusão - verificar se a LISTA está CHEIA
-			• Exclusão - verificar se a LISTA está VAZIA ou se o elemento a ser excluído NÃO
+			ï¿½ Inclusï¿½o - verificar se a LISTA estï¿½ CHEIA
+			ï¿½ Exclusï¿½o - verificar se a LISTA estï¿½ VAZIA ou se o elemento a ser excluï¿½do Nï¿½O
 			EXISTE na LISTA.
 			*/
 		
@@ -32,26 +32,26 @@ IMPLEMENTAÇÃO - VETOR DE 10 POSIÇÕES DE INTEIRO.
 		//FILA
 		
 			/*
-			Operações
-			• Inserção - sempre no FINAL
-			• Exclusão - sempre no TOPO
+			Operaï¿½ï¿½es
+			ï¿½ Inserï¿½ï¿½o - sempre no FINAL
+			ï¿½ Exclusï¿½o - sempre no TOPO
 			
 			Controles
-			• Inclusão - verificar se a FILA está CHEIA
-			• Exclusão - verificar se a FILA está VAZIA
+			ï¿½ Inclusï¿½o - verificar se a FILA estï¿½ CHEIA
+			ï¿½ Exclusï¿½o - verificar se a FILA estï¿½ VAZIA
 			*/
 		
 		
 		//PILHA
 		
 			/*
-			Operações
-			• Inserção - sempre no TOPO
-			• Exclusão - sempre no TOPO
+			Operaï¿½ï¿½es
+			ï¿½ Inserï¿½ï¿½o - sempre no TOPO
+			ï¿½ Exclusï¿½o - sempre no TOPO
 			
 			Controles
-			• Inclusão - verificar se a PILHA CHEIA
-			• Exclusão - verificar se a PILHA está VAZIA
+			ï¿½ Inclusï¿½o - verificar se a PILHA CHEIA
+			ï¿½ Exclusï¿½o - verificar se a PILHA estï¿½ VAZIA
 			*/
 		
 	
@@ -61,30 +61,245 @@ IMPLEMENTAÇÃO - VETOR DE 10 POSIÇÕES DE INTEIRO.
 //INFO:
 
 /*
-Start: 29/03/2022
-End: 31/03/2022
+Date: 01/04/2022
 Author: Tales Barbosa Rodrigues
 Github: github.com/tales-br/Estudos/C
 */
 
 #include <iostream>
-#include <stdlib.h>
 
 using namespace std;
 
 #define MAX 10
 
+//Globais
 int fila[MAX];
+int flagFila = 0;
+
 int pilha[MAX];
+int flagPilha = 0;
+
 int lista[MAX];
+int flagLista = 0;
+
+int start = 0;
 
 typedef enum opt
 {
-	PRIM, ULT, PESQ
+	PRIM, ULT, ORD
 }Opt;
+
+//ProtÃ³tipos
+void mostra (int *estrutura, int flag); //1 - ok
+void insere (int *estrutura, int dado, int *flag, Opt x); //4 - ok
+void retira (int *estrutura, int dado, int *flag, Opt x);
+void ordena (int *estrutura, int flag); //3 - ok
+void limpa (int *estrutura, int *flag); //2 - ok
 
 
 int main ()
 {
-	cout<<"Alo mundo";
+	limpa (pilha, &flagPilha);
+	insere (pilha, 1, &flagPilha, PRIM);
+	insere (pilha, 2, &flagPilha, PRIM);
+	insere (pilha, 3, &flagPilha, PRIM);
+	mostra (pilha, flagPilha);
+/*
+	cout<<endl;
+	insere (pilha, 4, &flagPilha, ULT);
+	
+	mostra (pilha, flagPilha);
+
+	cout<<endl;
+	insere (pilha, 0, &flagPilha, ORD);
+	insere (pilha, 5, &flagPilha, ULT);
+	mostra (pilha, flagPilha);
+	cout<<endl;
+	retira (pilha, 0, &flagPilha, PRIM);
+	retira (pilha, 0, &flagPilha, ULT);
+*/
+	retira (pilha, 2, &flagPilha, ORD);
+	retira (pilha, 20, &flagPilha, ORD);
+
+	//retira (pilha, 200, &flagPilha, ORD);
+	mostra (pilha, flagPilha);
+
+	cout<<flagPilha;
+
+	system("pause");
+
+	return 0;
+}
+
+
+void insere (int *estrutura, int dado, int *flag, Opt x)
+{
+	int aux;
+
+	if(*flag == MAX)
+	{
+		cout<<"\n\t\tEstrutura cheia!"; //nÃ£o deveria tratar a expception aqui (eu acho)... seria legar ter o CR nesse caso.. mas n tive tempo de implementar
+	}
+	else
+	{
+		if(*flag == 0)
+		{
+			estrutura[0] = dado;
+		}
+		else
+		{
+			switch (x)
+			{	//podia ter colocado outra funcao e diminuir o cod aqui
+				case PRIM:
+					for(int i = *flag; i >= 0; i--)
+					{
+						estrutura[i] = estrutura[i-1];
+					}
+					estrutura[0] = dado;
+					break;
+							
+				case ORD:
+					for(int i = *flag; i >= 0; i--)
+					{
+
+						estrutura[i] = estrutura[i-1];
+
+					}
+					estrutura[0] = dado;
+					ordena(estrutura, *flag+1);
+					break;
+
+				case ULT:
+					estrutura[*flag] = dado;
+					break;
+				
+				default:
+					break;
+			}
+		}
+
+		*flag = *flag + 1;
+	}
+}
+
+
+void limpa (int *estrutura, int *flag)
+{
+	*flag = 0;
+
+    for (int i=0; i < MAX; i++)
+    {
+    	estrutura[i]=NULL;//dÃ¡ no mesmo que zero =s
+    }
+
+}
+
+
+void mostra (int *estrutura, int flag)
+{
+	if(flag == 0)
+	{
+
+		cout<<"\n\t\tEstrutura Vazia!";
+
+	}
+	else
+	{
+
+    for (int i=0; i <= flag-1; i++)
+    {
+
+			cout<<"["<<i+1<<"] = "<<estrutura[i]<<endl;
+
+    }
+
+	}
+	cout<<endl;
+}
+
+
+//bbl srt a duras penas
+void ordena (int *estrutura, int flag)
+
+{
+	int i, j, aux;
+
+
+	for(j=flag-1; j>=1; j--)
+  	{ 
+    for(i=0; i<j; i++)
+    { 
+      if(estrutura[i] > estrutura[i+1])
+      { 
+        aux = estrutura[i]; 
+        estrutura[i] = estrutura[i+1]; 
+        estrutura[i+1] = aux; 
+      }
+    }
+  } 
+}
+
+
+void retira (int *estrutura, int dado, int *flag, Opt x)
+{
+	int aux;
+	int j = 0;
+
+	if(*flag == 0)
+	{
+		cout<<"\n\t\tEstrutura vazia!"; //nÃ£o deveria tratar a expception aqui (eu acho)... seria legar ter o CR nesse caso.. mas n tive tempo de implementar
+	}
+	else
+	{
+			switch (x)
+			{	//podia ter colocado outra funcao e diminuir o cod aqui
+				case PRIM:
+
+					for(int i = 0; i <= *flag; i++)
+					{
+						estrutura[i] = estrutura[i+1];
+					}
+
+					estrutura[*flag] = NULL;
+					break;
+							
+				//uma questÃ£o aqui... estou sempre indo de trÃ¡s para frente... dependendo da estrutura, posso acabar tirando o valor digitado mais recente (caso tenha outro dado igual)
+				case ORD:
+
+					for(int i = *flag; i >= 0; i--)
+					{
+						if(estrutura[i]==dado)
+						{
+							j++;
+
+							for(aux = i; aux <= *flag; aux++)
+							{
+								estrutura[aux] = estrutura[aux+1];
+							}
+
+							break;
+						}
+					}
+
+					estrutura[*flag] = NULL;
+					break;
+
+				case ULT:
+					estrutura[*flag] = NULL;
+					break;
+				
+				default:
+					break;
+			}
+		
+
+		if(j==0)
+		{
+			cout<<"\t\tValor nao encontrado!\n";
+		}
+		else
+		{
+			*flag = *flag - 1;
+		}
+	}
 }
