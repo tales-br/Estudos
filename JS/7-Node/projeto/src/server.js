3f62e684bcd7aedd09efe709bfb31c0068c4db9f
Tales <20765261+tales-br@users.noreply.github.com>
2022-08-24 17:09:58 -0300

    Alterado: Arquivo refatorado, para incluir métodos: PUT, DELETE e POST

--- JS/7-Node/projeto/src/server.js
+++ JS/7-Node/projeto/src/server.js
@@ -1,21 +1,45 @@
 const port = 3003
 
-const express = require ("express")
+const express = require("express")
 const app = express()
 
-//Atende a todas as URLS!
-app.use((rqst, rsp, nxt) => {
-    console.log("Middleware 1...")
-    nxt()
-})
+//Habilitar urlencoded
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+const bancoDeDados = require("./bancoDeDados")
 
 app.get("/produtos", (rqst, rsp, nxt) => {
-    console.log("Middleware 2...")
-    nxt()
+    rsp.send(bancoDeDados.getProdutos())
 })
 
-app.get("/produtos", (rqst, rsp, nxt) => {
-    rsp.send({nome: "Notebook", preco: 10000}) //o método send converte para JSON automaticamente
+//Pode ir encadeando os parâmtros como ex.: "/produtos/:id/:nome"
+app.get("/produtos/:id", (rqst, rsp, nxt) => {
+    rsp.send(bancoDeDados.getProduto(rqst.params.id))
+})
+
+app.post("/produtos", (rqst, rsp, nxt) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: rqst.body.nome,
+        preco: rqst.body.preco
+    })
+    rsp.send(produto) //Já convertido em JSON
+})
+
+app.delete("/produtos/:id", (rqst, rsp, nxt) => {
+    const produto = bancoDeDados.delProduto(rqst.params.id)
+    rsp.send(produto)
+})
+
+app.put("/produtos/:id", (rqst, rsp, nxt) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: rqst.params.id,
+        nome: rqst.body.nome,
+        preco: rqst.body.preco
+    })
+    rsp.send(produto)
 })
 
 app.listen(port, () => {
