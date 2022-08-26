8bd96c2586a41f42f17c21f8bb29a14de4466953
Tales <20765261+tales-br@users.noreply.github.com>
2022-08-26 13:57:52 -0300

    Adicionado: Uso de map em EMCA script

---
+++ JS/8-ENext/map.js
@@ -0,0 +1,28 @@
+//Par chave e valor ((ACEITA "QLQR COISA" como chave, mas não pode ser duplicada)) igual a JAVA e outros ...
+
+const tecnologias = new Map()
+tecnologias.set('react', { framework: false })
+tecnologias.set('angular', { framework: true })
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react').framework)
+
+const chavesVariadas = new Map([
+    [function () { }, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número'],
+])
+
+chavesVariadas.forEach((vl, ch) => {
+    console.log(ch, vl)
+})
+
+console.log(chavesVariadas.has(123))
+chavesVariadas.delete(123)
+console.log(chavesVariadas.has(123))
+console.log(chavesVariadas.size)
+
+chavesVariadas.set(123, 'a')
+chavesVariadas.set(123, 'b')
+chavesVariadas.set(456, 'b')
+console.log(chavesVariadas)
